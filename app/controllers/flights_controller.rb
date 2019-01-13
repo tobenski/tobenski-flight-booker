9a757ea714bfddab850fa6520e0c3362f1594a30
Knud Rishøj <tirdyr@tobenski.dk>
2019-01-13 15:54:53 +0100

    screen2 Done

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,8 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { |a| [a.name, a.id] }
-    @date_options = Flight.all.sorted.map{ |f| [f.date_formatted, f.date] }
-    @flighs = Flight.search(params)
+    @date_options = Flight.all.sorted.map{ |f| [f.date_formatted, f.date.to_date] }.uniq
+    #@fligths = Flight.search(params)
+    @flights = Flight.where(from_airport_id: params[:from_airport], to_airport_id: params[:to_airport]).order(:date)
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,20 +1,12 @@
 class Flight < ApplicationRecord
   belongs_to      :from_airport,      class_name: "Airport"
   belongs_to      :to_airport,        class_name: "Airport"
-
+  
   def date_formatted
     self.date.strftime("%a %b %d, %Y")
   end
 
   def self.sorted
     self.order(:date)    
-  end
-
-  def self.search(params)
-    if params
-      Flight.where(from_airport_id: params[:from_airport], to_airport_id: params[:to_airport])
-    else
-      Flight.none
-    end
   end
 end
